07483fdecef6148cdc28b7708455427363f51c3e
Chris Myers <cmyers78@gmail.com>
2016-08-05 10:43:18 -0600

    initial commit

---
+++ SwoleMate/SwoleMate/FirstViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FirstViewController.swift
+//  SwoleMate
+//
+//  Created by Christopher Myers on 8/5/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    
+
+
+}
+
---
+++ SwoleMate/SwoleMate/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwoleMate
+//
+//  Created by Christopher Myers on 8/5/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
