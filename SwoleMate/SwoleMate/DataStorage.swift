61109d5fafb016d169f074233b18f8bcf535d0de
Chris Myers <cmyers78@gmail.com>
2016-08-07 14:49:11 -0600

    map components in place.

--- SwoleMate/SwoleMate/DataStorage.swift
+++ SwoleMate/SwoleMate/DataStorage.swift
@@ -8,28 +8,20 @@
 
 import UIKit
 import MapKit
-import CoreLocation
 
 class DataStorage: NSObject {
     
-    var collected : Int = 0
-    
     static let sharedInstance = DataStorage()
+    
     private override init() {
         
     }
     
     var boxesArray = [Box]()
     
-    private var googleArray = [Box]()
-    
     func addMKBox(box : Box) {
         self.boxesArray.append(box)
         print(box.boxName + " MK")
-    }
-    
-    func removeGoogleBoxes() {
-        self.googleArray.removeAll()
     }
     
     func numberOfBoxes() -> Int {
@@ -43,115 +35,4 @@ class DataStorage: NSObject {
         return nil
     }
 
-    
-    func addGoogleBox(box : Box) {
-        
-        var isFound = false
-        
-        for boxes in boxesArray {
-            
-            //print("\(box.boxName.lowercaseString) != \(boxes.boxName.lowercaseString)")
-            if box.boxName.lowercaseString == boxes.boxName.lowercaseString {
-                isFound = true
-            }
-        }
-        
-        if isFound == false {
-            self.googleArray.append(box)
-        }
-        
-        print("count == \(self.googleArray.count)")
-        
-        for name in googleArray {
-            print("Google Array: \(name.boxName)")
-        }
-        
-        for nameMK in boxesArray {
-            print("MK Array: \(nameMK.boxName)")
-        }
-    }
-    
-//    func parseGoogleBoxes() {
-//        
-//        
-//        for box in googleArray {
-//            
-//            let request = MKLocalSearchRequest()
-//            request.naturalLanguageQuery = box.boxName
-//            
-//            let search = MKLocalSearch(request: request)
-//            search.startWithCompletionHandler{
-//                (response, error) in
-//                
-//                if let response = response {
-//                        
-//                    if response.mapItems.count > 0 {
-//                        
-//                        let item = response.mapItems[0]
-//                        
-//                        let theBox = Box()
-//                        
-//                        if let name = item.name {
-//                            theBox.boxName = name
-//                        }
-//                        
-//                        print(theBox.boxName)
-//                        
-//                        if let phone = item.phoneNumber {
-//                            theBox.boxPhone = phone
-//                        }
-//                        
-//                        if let webAddress = item.url {
-//                            theBox.boxURL = webAddress
-//                        }
-//                        
-//                        var foundAddress = true
-//                        
-//                        if let test = item.placemark.addressDictionary?["FormattedAddressLines"] as? NSArray{
-//                            
-//                            if test.count < 3 {
-//                                foundAddress = false
-//                            }
-//                            else if test.count == 3 {
-//                                
-//                                theBox.boxAddressStreet = test[0] as! String
-//                                theBox.boxAddressCSZ = test[1] as! String
-//                                theBox.boxAddressCountry = test[2] as! String
-//                                
-//                            } else {
-//                                theBox.boxAddressStreet = test[0] as! String
-//                                theBox.boxAddressSuite = test[1]  as! String
-//                                theBox.boxAddressCSZ = test[2] as! String
-//                                theBox.boxAddressCountry = test[3] as! String
-//                            }
-//                        }
-//                        
-//                        theBox.boxLat = item.placemark.coordinate.latitude
-//                        
-//                        theBox.boxLong = item.placemark.coordinate.longitude
-//                        
-//                        if foundAddress == true {
-//                            self.boxesArray.append(theBox)
-//                            theBox.logItems()
-//                            print()
-//                            
-//                            NSNotificationCenter.defaultCenter().postNotificationName(kNOTIFY, object: nil)
-//                            // add notification here - post notification here
-//                            //print("The box added is: \(theBox.boxName)")
-//                        }
-//                        
-//                    
-//                    else {
-//                        print("Search results not found for: \(box.boxName)")
-//                        // remove from google array
-//                    }
-//                    }
-//                } else {
-//                    print("There was an error searching for: \(request.naturalLanguageQuery) error: \(error)")
-//                    return
-//                }
-//            }
-//        }
-//        
-//    }
 }
--- SwoleMate/SwoleMate/FindBoxViewController.swift
+++ SwoleMate/SwoleMate/FindBoxViewController.swift
@@ -15,7 +15,9 @@ class FindBoxViewController: UIViewController, MKMapViewDelegate, CLLocationMana
     var mapName : String = ""
     
     var locationManager = CLLocationManager()
-
+    
+    let controller = APIController()
+    
     @IBOutlet weak var mapView: MKMapView!
 
     override func viewDidLoad() {
@@ -24,9 +26,8 @@ class FindBoxViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         self.locationManager.delegate = self
         self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locationManager.requestAlwaysAuthorization()
-        
         self.findUserLocation()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     func findUserLocation() {
@@ -40,11 +41,130 @@ class FindBoxViewController: UIViewController, MKMapViewDelegate, CLLocationMana
         }
     }
     
+    func dropPin() {
+        print("drop pin called")
+        for box in DataStorage.sharedInstance.boxesArray {
+            
+            self.addPin(box.boxLat, pinLong: box.boxLong, title: box.boxName, address: box.boxAddressStreet + " " + box.boxAddressCSZ)
+        }
+    }
+    
+    // MARK : Annotations
+    
+    func addPin(pinLat : Double, pinLong : Double, title : String, address : String) {
+        
+        let location = CLLocationCoordinate2D(latitude: pinLat, longitude: pinLong)
+        let annotation = CustomBoxMKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = title
+        annotation.subtitle = address
+        
+        self.mapView.addAnnotation(annotation)
+    }
+
+    
     // Delegate methods
     
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         //self.locationManager.startUpdatingLocation()
         self.findUserLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count > 0 {
+            let location = locations.first
+            print(location?.coordinate.latitude)
+            print(location?.coordinate.longitude)
+            let span = MKCoordinateSpan(latitudeDelta: 0.5, longitudeDelta: 0.5)
+            if let center = location?.coordinate {
+                let region = MKCoordinateRegion(center: center, span: span)
+                
+                self.mapView.setRegion(region, animated: true)
+                self.mapView.showsUserLocation = true
+                //self.locationManager.startUpdatingLocation()
+            }
+        }
+        
+        if let loc = locationManager.location {
+            
+            print(loc.coordinate)
+            
+            let lat = loc.coordinate.latitude
+            let long = loc.coordinate.longitude
+            
+            controller.findMKBox(lat, long: long)
+        }
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.dropPin), name: kNOTIFY, object: nil)
+        
+    }
+    
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        
+        if annotation.isKindOfClass(CustomBoxMKPointAnnotation) {
+            let identifier = "kettleBell"
+            
+            var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+            
+            
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            
+            if let annotationView = annotationView {
+                
+                annotationView.canShowCallout = true
+                
+                let imageView = UIImageView(frame: CGRectMake(0, 0, 24, 24))
+                imageView.contentMode = .ScaleAspectFit
+                
+                imageView.image = UIImage(named: "kettlebellx24")
+                
+                let smallSquare = CGSize(width: 30, height: 30)
+                let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+                button.setBackgroundImage(UIImage(named: "carX32"), forState: .Normal)
+                button.addTarget(self, action: #selector(self.getDirections), forControlEvents: .TouchUpInside)
+                annotationView.leftCalloutAccessoryView = button
+                
+                annotationView.image = imageView.image
+                
+                return annotationView
+            }
+        }
+        return nil
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView) {
+        
+        //print("POPUP ANNOTATE")
+        // I need to set selectedPin equal to the pin's coordinates
+        if let coordinate = view.annotation?.coordinate {
+            
+            let placemark = MKPlacemark(coordinate: coordinate, addressDictionary: nil)
+            self.selectedPin = placemark
+            
+            if let mName = view.annotation?.title {
+                self.mapName = mName!
+            }
+        }
+    }
+    
+    func getDirections() {
+        print("button tapped")
+        
+        let mapItem = MKMapItem(placemark: self.selectedPin!)
+        mapItem.name = self.mapName
+        if let lat = self.selectedPin?.coordinate.latitude, long = self.selectedPin?.coordinate.longitude {
+            
+            let regionDistance : CLLocationDistance = 7500
+            
+            let coord = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let regionSpan = MKCoordinateRegionMakeWithDistance(coord, regionDistance, regionDistance)
+            
+            let options = [MKLaunchOptionsMapCenterKey: NSValue(MKCoordinate: regionSpan.center), MKLaunchOptionsMapSpanKey: NSValue(MKCoordinateSpan: regionSpan.span), MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving ]
+            
+            mapItem.openInMapsWithLaunchOptions(options)
+        }
     }
     
     func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
