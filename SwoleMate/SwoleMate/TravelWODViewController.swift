291c0099f5b26028db3ecc17a96af1ae0ae65ab2
Chris Myers <cmyers78@gmail.com>
2016-08-08 09:32:07 -0600

    monday morning commit  - tableview boiler code setup

--- SwoleMate/SwoleMate/TravelWODViewController.swift
+++ SwoleMate/SwoleMate/TravelWODViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class TravelWODViewController: UIViewController {
+class TravelWODViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.tableView.reloadData()
+
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 1
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        
     }
     
 }
