8db3ee3a4a6ea858ae11fce61ebfe9ca687bd545
Chris Myers <cmyers78@gmail.com>
2016-08-08 16:40:48 -0600

    Custom table view cell...

---
+++ SwoleMate/SwoleMate/TravelWODTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TravelWODTableViewCell.swift
+//  SwoleMate
+//
+//  Created by Christopher Myers on 8/8/16.
+//  Copyright © 2016 Dragoman Developers, LLC. All rights reserved.
+//
+
+import UIKit
+
+class TravelWODTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
